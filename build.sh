4ef76dedf5abfe4b4390e5880c38cad51f118c8d
Peter L <plix1014@gmail.com>
2025-12-28 09:10:30 +0100

    docker build helper script

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+ENV=.env
+
+if [ -f "$ENV" ]; then
+    . $ENV
+fi
+
+if [ $# -ne 1 ]; then
+    echo
+    echo "usage: ${0##*/} <inc|full>"
+    echo
+    exit 1
+fi
+
+OPTS=
+
+if [ "$1" = "full" ]; then
+    OPTS="--no-cache"
+fi
+
+echo "docker build $OPTS --build-arg="CONT_VER=$TAG" -t ${IMAGE_NAME}:$TAG"
+time docker build $OPTS --build-arg="CONT_VER=$TAG" -t ${IMAGE_NAME}:$TAG  .
+
